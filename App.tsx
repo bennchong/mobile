5a684fcbaa54acb5cb4f04111df011ddda8f11e1
Benjamin Chong <xyers@hotmail.com>
2019-07-09 16:27:32 +0800

    Working Barcode scanner under components

--- App.tsx
+++ App.tsx
@@ -1,19 +1,22 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import BarcodeScannerExample from "./components/BarcodeScannerExample";
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "transparent",
     alignItems: "center",
     justifyContent: "center"
   }
 });
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <BarcodeScannerExample />
+      </View>
+    );
+  }
 }
