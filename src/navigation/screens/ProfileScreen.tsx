981fcd10b3b39001313dc4719d2046273c7a0ece
Benjamin Chong <xyers@hotmail.com>
2019-07-10 12:22:14 +0800

    Working params passed to profile

--- src/navigation/screens/ProfileScreen.tsx
+++ src/navigation/screens/ProfileScreen.tsx
@@ -3,9 +3,12 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default class ProfileScreen extends React.Component {
   render() {
+    const { navigation } = this.props;
+    const barcodeStatus = navigation.getParam('barcode_status', 'Not Captured');
     return (
       <View style={styles.container}>
         <Text>Profile Page</Text>
+        <Text>Have you captured QR code yet?: {JSON.stringify(barcodeStatus)}</Text>
       </View>
     );
   }
