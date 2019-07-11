fd5bf84812019798b7f4378b9bac26c5eaf8ad86
Benjamin Chong <xyers@hotmail.com>
2019-07-11 09:50:55 +0800

    Added interfaces and state to App class

--- App.tsx
+++ App.tsx
@@ -4,7 +4,26 @@ import NavigationService from './src/navigation/NavigationService';
 
 const ThemeContext = React.createContext('light');
 
-export default class App extends React.Component {
+interface MyProps{}
+interface MyState{
+  test: boolean
+}
+
+export default class App extends React.Component <MyProps, MyState> {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      test: false,
+    }
+  }
+
+  changeTestState = () => {
+    this.setState({
+      test: !this.state.test
+    })
+  }
+
   render() {
     return (
       <ThemeContext.Provider value="dark">
