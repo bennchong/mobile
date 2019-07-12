f092ede862246a5ffb4de552b26ef82c3839a1c4
Benjamin Chong <xyers@hotmail.com>
2019-07-12 16:25:45 +0800

    Profile page to use context to acquire AppStore state

--- src/navigation/screens/ProfileScreen.tsx
+++ src/navigation/screens/ProfileScreen.tsx
@@ -1,16 +1,20 @@
 import React from '../../../node_modules/react';
-import { StyleSheet, Text, View } from 'react-native';
 import Profile from "./Stacks/profile/Profile";
 import NoProfile from "./Stacks/profile/NoProfile";
+import AppContext from "../../components/AppStore";
 
 interface MyProps{
   testState: boolean
 }
 
+
 export default class ProfileScreen extends React.Component<MyProps,{}> {
-  
+
+// Read up on context, it links to AppStore Class apparently, and this.context would allow us to acess AppStore methods and variables  
+static contextType = AppContext; 
+
   render() {
-    if(this.props.testState) {
+    if(this.context.test) {
       return(
         <Profile />
       );
--- src/navigation/screens/ProfileTab.tsx
+++ src/navigation/screens/ProfileTab.tsx
@@ -1,14 +1,10 @@
 import React from '../../../node_modules/react';
-import { StyleSheet, Text, View } from 'react-native';
 import ProfileScreen from './ProfileScreen';
-import AppContext from "../../components/AppStore";
 
 export default class ProfileTab extends React.Component {
   render() {
-      return (
-          <AppContext.Consumer>
-              {({test}) => (<ProfileScreen testState={test}/>)}
-          </AppContext.Consumer>         
-      );
+        return (
+            <ProfileScreen />     
+        );
   }
 }
