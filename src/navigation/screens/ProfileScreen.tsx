43fea611723001ff4801510f93fb7753f6e2f473
Benjamin Chong <xyers@hotmail.com>
2019-07-15 13:04:46 +0800

    Added eventListener of profileTab

--- src/navigation/screens/ProfileScreen.tsx
+++ src/navigation/screens/ProfileScreen.tsx
@@ -2,16 +2,27 @@ import React from '../../../node_modules/react';
 import Profile from "./Stacks/profile/Profile";
 import NoProfile from "./Stacks/profile/NoProfile";
 import AppContext from "../../components/AppStore";
+import { withNavigation } from "react-navigation";
+import { Alert } from 'react-native';
 
-interface MyProps{
-  testState: boolean
-}
-
-
-export default class ProfileScreen extends React.Component<MyProps,{}> {
+class ProfileScreen extends React.Component<{},{}> {
 
 // Read up on context, it links to AppStore Class apparently, and this.context would allow us to acess AppStore methods and variables  
 static contextType = AppContext; 
+  
+  componentDidMount() {
+    const { navigation } = this.props;
+    this.focusListener = navigation.addListener("didFocus", () => {
+      // The screen is focused
+      // Call any action
+      Alert.alert("Running re-verification placeholder function")
+    });
+  }
+
+  componentWillUnmount() {
+    // Remove the event listener
+    this.focusListener.remove();
+  }
 
   render() {
     if(this.context.test) {
@@ -26,3 +37,5 @@ static contextType = AppContext;
     }
   }
 }
+
+export default withNavigation(ProfileScreen);
