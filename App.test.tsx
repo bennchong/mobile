3ca35ee8699e0ca546788149da5324ba4c407d38
Benjamin Chong <xyers@hotmail.com>
2019-07-16 11:06:53 +0800

    Updated snapshot test

--- App.test.tsx
+++ App.test.tsx
@@ -2,10 +2,19 @@ import React from "react";
 import renderer from "react-test-renderer";
 
 import App from "./App";
+import SettingsTab from './src/navigation/screens/SettingsTab';
 
-describe("<App />", () => {
+// Test cases shouldn't include snapshotting the whole app 
+// describe("<App />", () => {
+//   it("should render without crashing", () => {
+//     const tree = renderer.create(<App />).toJSON();
+//     expect(tree).toMatchSnapshot();
+//   });
+// });
+
+describe("<SettingsTab />", () => {
   it("should render without crashing", () => {
-    const tree = renderer.create(<App />).toJSON();
+    const tree = renderer.create(<SettingsTab />).toJSON();
     expect(tree).toMatchSnapshot();
   });
 });
