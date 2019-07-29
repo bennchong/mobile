c5fd719b3d71d3198106ad4531979d9e36b1d463
yk <yykcool@hotmail.com>
2019-07-29 13:35:11 +0800

    fixed lint errors

--- App.test.tsx
+++ App.test.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import renderer from "react-test-renderer";
-import { SettingsTab } from "./src/navigation/tabs/Settings";
 import { createAppContainer } from "react-navigation";
+import { SettingsTab } from "./src/navigation/tabs/Settings";
 
 describe("<SettingsTab />", () => {
   it("should render without crashing", () => {
-    let WrappedSettings = createAppContainer(SettingsTab);
+    const WrappedSettings = createAppContainer(SettingsTab);
     const tree = renderer.create(<WrappedSettings />).toJSON();
     expect(tree).toMatchSnapshot();
   });
