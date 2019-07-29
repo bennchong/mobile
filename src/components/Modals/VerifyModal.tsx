47ccba298a845a4733b12255cfa190f7d40364e5
samuellye98 <sl2982@cornell.edu>
2019-07-29 14:38:31 +0800

    comments part i

--- src/components/Modals/VerifyModal.tsx
+++ src/components/Modals/VerifyModal.tsx
@@ -4,7 +4,7 @@ import { AntDesign } from "@expo/vector-icons";
 import PropTypes from "prop-types";
 import { styles } from "./modalStyles";
 
-const VerifyModal = ({ onPress, showModal }) => {
+export const VerifyModal = ({ onPress, showModal }) => {
   return (
     <Modal visible={showModal} transparent={true} animationType="fade">
       <View style={styles.overlay}>
@@ -12,10 +12,7 @@ const VerifyModal = ({ onPress, showModal }) => {
           <View style={styles.modal}>
             <AntDesign name="checkcircle" size={50} color="#32CD32" />
             <Text style={styles.modalText}>Digital work pass saved!</Text>
-            <TouchableOpacity
-              style={styles.modalButton}
-              onPress={() => onPress()}
-            >
+            <TouchableOpacity style={styles.modalButton} onPress={onPress}>
               <Text>View profile</Text>
             </TouchableOpacity>
           </View>
@@ -24,8 +21,6 @@ const VerifyModal = ({ onPress, showModal }) => {
     </Modal>
   );
 };
-
-export { VerifyModal };
 
 VerifyModal.propTypes = {
   onPress: PropTypes.func,
