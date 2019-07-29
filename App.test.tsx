5298e15b9f99944363f68d695390c778a231853c
yk <yykcool@hotmail.com>
2019-07-29 13:34:25 +0800

    fixed app.test.tsx to account for SettingsTab being a navigator instead of a component, updated snapshots

--- App.test.tsx
+++ App.test.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import renderer from "react-test-renderer";
-
 import { SettingsTab } from "./src/navigation/tabs/Settings";
+import { createAppContainer } from "react-navigation";
 
 describe("<SettingsTab />", () => {
   it("should render without crashing", () => {
-    const tree = renderer.create(<SettingsTab />).toJSON();
+    let WrappedSettings = createAppContainer(SettingsTab);
+    const tree = renderer.create(<WrappedSettings />).toJSON();
     expect(tree).toMatchSnapshot();
   });
 });
